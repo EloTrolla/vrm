f0224cab30c4d1fed2982e113a7d68cc23fe6ecc
Elo Trolla <elo.trolla@khk.ee>
2017-08-30 12:31:04 +0300

    Created database connection

--- web/index.php
+++ web/index.php
@@ -1,5 +1,6 @@
 <?php
 // web/index.php
+// using Silex framework
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
@@ -8,10 +9,20 @@ $app = new Silex\Application();
 
 $app['debug'] = true;
 
-
+// using Twig template framework
 $app->register(new Silex\Provider\TwigServiceProvider(), [
     'twig.path' => __DIR__.'/../views',
 ]);
+
+
+// Database configuration  DBAL
+$app->register(new Silex\Provider\DoctrineServiceProvider(), [
+    'db.options' => [
+        'driver' => 'pdo_sqlite',
+        'path' => __DIR__.'/../database/app.db',
+    ],
+]);
+
 
 
 
