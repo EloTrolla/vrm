00425dbfd275127e9c3c0964b398606ff2f5c3af
Elo Trolla <elo.trolla@khk.ee>
2017-08-30 12:44:41 +0300

    Created bookings table

--- web/index.php
+++ web/index.php
@@ -25,6 +25,27 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), [
 
 
 
+// Creating a table if it doesn't exist yet
+if (!$app['db']->getSchemaManager()->tablesExist('bookings')) {
+    $app['db']->executeQuery("CREATE TABLE bookings (
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        firstName VARCHAR(40) NOT NULL,
+        lastName VARCHAR(40) NOT NULL,
+        phone VARCHAR(10) NOT NULL,
+        email VARCHAR(20) DEFAULT NULL,
+        birthday DATE NOT NULL,
+        startDate DATE NOT NULL,
+        endDate DATE NOT NULL,
+        arrivalTime TIME DEFAULT NULL,
+        additionalInformation TEXT,
+        nrOfPeople INT NOT NULL,
+        payingMethod VARCHAR(10) NOT NULL
+    );");
+}
+
+
+
+
 
 $app->get('/bookings/create', function () use ($app) { // http://localhost:8000/hello  return hello world
 
